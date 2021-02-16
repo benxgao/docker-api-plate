b6af5079cc68525b122a62861bbafe821ee35e23
xingbing Gao <xingbingao@gmail.com>
2021-02-17 11:50:20 +1300

    Make changes from template to fit project purpose

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,18 @@
 import prettierRules from './.prettierrc.json';
 
 module.exports = {
-  env: { jest: true, browser: true },
+  env: { jest: true, node: true },
   extends: [
     'eslint:recommended'
   ],
   plugins: ['jest', 'prettier'],
   parserOptions: {
-    parser: 'babel-eslint', // the typescript-parser for eslint, instead of tslint
-    sourceType: 'module', // allow the use of imports statements
-    ecmaVersion: 2020, // allow the parsing of modern ecmascript
-    ecmaFeatures: {
-      jsx: true
-    }
+    parser: 'babel-eslint',
+    sourceType: 'module',
+    ecmaVersion: 2020,
   },
   settings: {
-    'import/extensions': ['.js', 'jsx']
+    'import/extensions': ['.js']
   },
   rules: {
     'prettier/prettier': ['error', prettierRules],
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Boilerplate of Node.js application
+# Docker-api-plate
+
+A template of serving Node.js API in Docker and publishing Docker image to DockerHub via Github Actions
+
+## Get started
+
+```bash
+
+```
